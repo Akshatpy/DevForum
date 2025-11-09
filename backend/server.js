b7a53b6843db5ed733e9e74f42cd1ffa06c64814
Akshat Sharma <akshatomega@gmail.com>
2025-11-09 17:32:44 +0530

    wrong syntax app push

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use('/api/questions', questionRoutes);
 app.use('/api/answers', answerRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/tags', tagRoutes);
-app.push('/api/communities', communityRoutes);
+app.use('/api/communities', communityRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
