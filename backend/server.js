5a139b910f8b8dc8a5805962fcea7a88225ef1b8
Akshat Sharma <akshatomega@gmail.com>
2025-11-09 17:02:51 +0530

    vercel and atlas connection fixes

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 // Database connection
 mongoose
-  .connect(process.env.MONGO_URI || 'mongodb://localhost:27017/dev-connect', {
+  .connect(process.env.MONGO_URI , {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
