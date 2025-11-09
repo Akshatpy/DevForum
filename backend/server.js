67ba36855d19eab6194237ae44dadfe95aeddc92
Akshat Sharma <akshatomega@gmail.com>
2025-11-09 17:25:37 +0530

    switching to render for hosting

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const answerRoutes = require('./routes/answers');
 const userRoutes = require('./routes/users');
 const tagRoutes = require('./routes/tags');
 const communityRoutes = require('./routes/communities');
+
 const app = express();
 
 // Middleware
@@ -29,9 +30,7 @@ app.use('/api/questions', questionRoutes);
 app.use('/api/answers', answerRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/tags', tagRoutes);
-app.use('/api/communities', communityRoutes);
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
-module.exports = app;
+app.push('/api/communities', communityRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
